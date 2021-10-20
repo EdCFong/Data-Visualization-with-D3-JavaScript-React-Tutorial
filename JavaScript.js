e56cd9eae03a3b57892919b1e84f0b9de09c064c
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2021-10-19 22:50:56 -0400

    Update October 19, 2021

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const arc = d3.arc();
+
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const strockWidth = 20;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 50;
+const mouthArc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(100)
+    .startAngle(0)
+    .endAngle(Math.PI / 2);
 
 const App = () => (
-    <h1>Hello J</h1>
+    <svg width={width} height={height}>
+        <g>
+            <circle
+                cx={centerX}
+                cy={centerY}
+                r={centerY - strockWidth / 2}
+                fill="yellow"
+                stroke="black"
+                stroke-width={strockWidth}
+            />
+            <circle
+                cx={centerX - eyeOffsetX}
+                cy={centerY - eyeOffsetY}
+                r={eyeRadius}
+            />
+            <circle
+                cx={centerX + eyeOffsetX}
+                cy={centerY - eyeOffsetY}
+                r={eyeRadius}
+            />
+            <path d={mouthArc()} />
+
+
+        </g>
+    </svg>
 );
 
 
